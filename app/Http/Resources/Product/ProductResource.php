403af0fd2be052bbecc7ff259eb68c1637f87d9a
elyurias <irving2323@gmail.com>
2019-01-17 18:11:16 -0600

    Envio de la informacion y envio de urls para manejar los comentarios

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -15,11 +15,16 @@ class ProductResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'name'=>$this->name,
-            'description'=>$this->detail,
-            'price'=>$this->price,
-            'stock'=>$this->stock,
-            'discount'=>$this->discount
+            'name'          =>$this->name,
+            'description'   =>$this->detail,
+            'price'         =>$this->price,
+            'stock'         =>$this->stock == 0 ? 'Out to stock' : $this->stock,
+            'discount'      =>$this->discount,
+            'totalPrice'    =>round((1 - ($this->discount/100)) * $this->price, 2),
+            'raiting'       =>$this->reviews->count() > 0 ? round($this->reviews->sum('star')/$this->reviews->count(),2) : 'Not raiting yet',
+            'href'          =>[
+                'reviews' => route('reviews.index', $this->id)
+            ]
         ];
     }
 }
